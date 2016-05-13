9b350c615e734762229084990932d59c3d0aaf07
jonpasski <jon@passki.us>
2016-05-12 18:46:51 -0700

    Initial commit

---
+++ src/main/java/us/passki/tutorial/zap/TutorialExtension.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package us.passki.tutorial.zap;
+
+import org.parosproxy.paros.extension.ExtensionAdaptor;
+
+/**
+ * @author jonpasski
+ *
+ */
+public class TutorialExtension extends ExtensionAdaptor {
+
+	/* (non-Javadoc)
+	 * @see org.parosproxy.paros.extension.Extension#getAuthor()
+	 */
+	public String getAuthor() {
+		// TODO Auto-generated method stub
+		return "Jon Passki";
+	}
+}
